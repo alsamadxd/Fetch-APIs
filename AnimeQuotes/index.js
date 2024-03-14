3f9ae75da9b188e54dca462f0d6c72ba3f79b801
unknown <alsamad0005@gmail.com>
2024-03-14 13:51:52 +0530

    Anime Quotes

---
+++ AnimeQuotes/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const animeName = document.querySelector(".anime");
+const quote = document.querySelector(".quotes");
+const author = document.querySelector(".author");
+
+function pushElement(dataObj) {
+  document.querySelector(".anime").textContent = dataObj.anime;
+  document.querySelector(".quotes").textContent = dataObj.quote;
+  document.querySelector(".author").textContent = ` --${dataObj.character}`;
+  //   console.log(dataObj);
+  //   console.log(dataObj.anime);
+  //   console.log(dataObj.quote);
+  //   console.log(dataObj.character);
+}
+function getData() {
+  return fetch("https://animechan.xyz/api/random").then((response) =>
+    response.json()
+  );
+}
+
+document.querySelector(".btn").addEventListener("click", () => {
+  getData().then((res) => {
+    pushEelment(res);
+  });
+});
